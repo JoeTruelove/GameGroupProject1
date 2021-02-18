0533d2c68c55515215d2880c2a98ba1259294b8b
JoeTruelove <43154676+JoeTruelove@users.noreply.github.com>
2021-02-17 21:20:58 -0500

    New Build

--- GroupGameProjectPrototype/Assets/Scripts/PlayerController.cs
+++ GroupGameProjectPrototype/Assets/Scripts/PlayerController.cs
@@ -11,23 +11,32 @@ public class PlayerController : MonoBehaviour
     private float yRot;
     public float speed = 1;
     public float horizontalSpeed = 1;
+    public float maxSpeed = 1;
     public bool onWall = false;
     public float raycastLineDist = 1;
     public bool frontWall = false;
-    public bool death = false;
     public bool startMoving = false;
     public int timesJumped = 0;
-    public int currentLevel = 1;
+    public int currentLevel = 0;
+    public bool onWallGravity = true;
+    public float onWallGravitySpeed = .001f;
+    public bool deathAllowed = true;
+    public List<GameObject> Spawns;
+
+    private bool death = false;
     private bool justPortaled = false;
-    
-    private Animator anim;
+    private bool justLeftWalled = false;
+    private bool justRightWalled = false;
+
+    //private Animator anim;
     private Rigidbody rigidBody;
+    public GameObject playerBody;
 
     // Use this for initialization
     void Start()
     {
 
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
         startMoving = false;
 
@@ -67,7 +76,7 @@ public class PlayerController : MonoBehaviour
         isMoving = false;
 
         //Jumping code
-        if ((Input.GetKeyDown(KeyCode.Space) && (isGrounded || (timesJumped < 2 && onWall))))
+        if ((Input.GetKeyDown(KeyCode.Space) && (isGrounded || (timesJumped < 2 && onWall))) && startMoving)
         {
             //transform.Translate(Vector3.up * jumpHeight);
             //rigidBody.AddForce(transform.TransformDirection(Vector3.up) * 5);
@@ -80,23 +89,37 @@ public class PlayerController : MonoBehaviour
         if (!frontWall && startMoving)
         {
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            playerBody.GetComponent<Animator>().SetBool("Running", true);
+        }
+        if(onWall)
+        {
+            Vector3 pos = new Vector3(transform.position.x, transform.position.y - onWallGravitySpeed, transform.position.z);
+            transform.position = pos;
         }
         
-        
         //Moving left and right
-        if ((Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f && !onWall) && startMoving)
+        if ((Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) && startMoving)
         {
             //transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * playerSpeed);
             rigidBody.velocity += transform.right * Input.GetAxisRaw("Horizontal") * horizontalSpeed;
+            /*if(rigidBody.velocity.sqrMagnitude < maxSpeed && transform.right)
+            {
+                rigidBody.velocity += transform.right * Input.GetAxisRaw("Horizontal") * horizontalSpeed;
+            }*/
+            
             isMoving = true;
         }
 
+        //Turn on or off onWallGravity
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            onWallGravity = !onWallGravity;
+        }
 
         //Reset Player Position
         if (Input.GetKeyDown(KeyCode.R))
         {
-            this.gameObject.transform.position = new Vector3(23.019f, 0.231f, 25.645f);
-            startMoving = false;
+            changeLevel(currentLevel);
         }
 
         //Gravity Code
@@ -117,33 +140,57 @@ public class PlayerController : MonoBehaviour
             this.gameObject.transform.Rotate(0, 0, 270);
         }*/
 
-        //Restart Code
-        if(death && currentLevel == 1)
-        {
-            this.gameObject.transform.position = new Vector3(23.019f, 0.231f, 25.645f);
-            death = false;
-            startMoving = false;
-        }
-        else if (death && currentLevel == 2)
-        {
-            this.gameObject.transform.position = new Vector3(281.98f, -0.37f, -80.2f);
-            death = false;
-            startMoving = false;
-        }
-
-        if(currentLevel == 2 && justPortaled)
-        {
-            this.gameObject.transform.position = new Vector3(281.98f, -0.37f, 80.2f);
-            startMoving = false;
-            justPortaled = false;
-        }
-
-        //wanim.SetBool("isMoving", isMoving);
+        
 
         
+
+        //wanim.SetBool("isMoving", isMoving);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            changeLevel(0);
+            currentLevel = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            changeLevel(1);
+            currentLevel = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            changeLevel(2);
+            currentLevel = 2;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            changeLevel(3);
+            currentLevel = 3;
+        }
     }
 
-
+    public void changeLevel(int i)
+    {
+        GameObject o = Spawns[i];
+        this.gameObject.transform.position = o.transform.position;
+        death = false;
+        startMoving = false;
+        justPortaled = false;
+        isGrounded = true;
+        rigidBody.velocity = Vector3.zero;
+        playerBody.GetComponent<Animator>().SetBool("Running", false);
+        if(currentLevel == 0 || currentLevel == 3)
+        {
+            speed = 8;
+        }
+        if(currentLevel == 1)
+        {
+            speed = 12;
+        }
+        if(currentLevel == 2)
+        {
+            speed = 9;
+        }
+    }
     //consider what the object is collider with
     void OnCollisionEnter(Collision other)
     {
@@ -151,22 +198,46 @@ public class PlayerController : MonoBehaviour
         {
             isGrounded = true;
             timesJumped = 0;
+            onWall = false;
+            justLeftWalled = false;
+            justRightWalled = false;
         }
 
-        if (other.gameObject.tag == "Wall")
+        if (other.gameObject.tag == "LeftWall")
         {
             onWall = true;
+            if(justRightWalled && timesJumped == 2)
+            {
+                timesJumped = 1;
+            }
+            justRightWalled = false;
+            justLeftWalled = true;
+        }
+        
+        if (other.gameObject.tag == "RightWall")
+        {
+            onWall = true;
+            if (justLeftWalled && timesJumped == 2)
+            {
+                timesJumped = 1;
+            }
+            justLeftWalled = false;
+            justRightWalled = true;
         }
 
         if (other.gameObject.tag == "Death")
         {
-            death = true;
+            if(deathAllowed)
+            {
+                death = true;
+                changeLevel(currentLevel);
+            }
         }
 
         if (other.gameObject.tag == "Portal")
         {
-            currentLevel = 2;
-            justPortaled = true;
+            currentLevel++;
+            changeLevel(currentLevel);
         }
     }
 
@@ -178,9 +249,18 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
         }
 
-        if (other.gameObject.tag == "Wall")
+        if (other.gameObject.tag == "LeftWall")
         {
             onWall = false;
+            justLeftWalled = true;
+            justRightWalled = false;
+        }
+
+        if (other.gameObject.tag == "RightWall")
+        {
+            onWall = false;
+            justRightWalled = true;
+            justLeftWalled = false;
         }
 
         if (other.gameObject.tag == "Death")
